ce7461fb410aa4a821e9fefa1b98bff243144be6
Geert Huls <geert.huls@macadam.eu>
2016-01-20 22:14:41 +0100

    Read me file

--- server.js
+++ server.js
@@ -5,9 +5,3 @@ let app = express();
 app.get('/', (req, res) => res.send('Hello express!'));
 
 app.listen(3000);
-
-
-
-//http://stackoverflow.com/questions/33624104/how-do-i-setup-babel-6-with-node-js-to-use-es6-in-my-server-side-code
-//http://stackoverflow.com/questions/28782656/how-to-run-node-js-app-with-es6-features-enabled/29415291#29415291
-
