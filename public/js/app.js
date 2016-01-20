b6e178b71829191f45570af432fb6a4fc94dcef2
Geert Huls <geert.huls@macadam.eu>
2016-01-20 23:33:14 +0100

    Using jsx instead of plain javascript

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,8 @@ var ReactDOM = require("react-dom");
 
 var Hello = React.createClass({
 	render: function() {
-		return React.createElement("h3", null, "Hello React");
+		return <h3>Hello React</h3>;
 	}
 });
 
-ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
+ReactDOM.render(<Hello />, document.getElementById('react'));
