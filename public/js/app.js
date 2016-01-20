84568341e76a4a83b0851dc93f4107fc6b098dc7
Geert Huls <geert.huls@macadam.eu>
2016-01-20 22:50:34 +0100

    Test react component

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
-console.log("Test js");
+var Hello = React.createClass({
+	render: function() {
+		return React.createElement("h3", null, "Hello React");
+	}
+});
+
+ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
