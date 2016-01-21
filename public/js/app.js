223626bd743e035fabb4971bb6412989c022261a
Geert Huls <geert.huls@macadam.eu>
2016-01-21 21:18:10 +0100

    Moved hello component into seperate file

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Hello extends React.Component {
-	render(){
-		return <h3>Hello ES6</h3>;
-	}
-}
+import Hello from "./components/Hello";
 
 ReactDOM.render(<Hello />, document.getElementById('react'));
