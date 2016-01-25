7498b8cdfd46ec5e374d5c4baf164ed99d29d437
Geert Huls <geert.huls@macadam.eu>
2016-01-25 23:27:35 +0100

    Finally render all content in the view

--- js/components/Main.js
+++ js/components/Main.js
@@ -30,12 +30,16 @@ export default class Main extends React.Component {
 	}
 
 	render() {
+		let content = this.state.links.map(link => {
+			return 	<li key={link._id}>
+						<a href={link.url}>{link.title}</a>
+					</li>;
+		});
 		return (
 			<div>
 				<h3>links</h3>
 				<ul>
-					<li>Link...</li>
-					<li>Link...</li>
+					{content}
 				</ul>
 			</div>
 		);
