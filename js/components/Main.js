fdddd078d65b1004ad7f8333fc8615f563fbb878
Geert Huls <geert.huls@macadam.eu>
2016-01-28 22:36:44 +0100

    State and onChange turned into properties in main component

--- js/components/Main.js
+++ js/components/Main.js
@@ -15,13 +15,8 @@ export default class Main extends React.Component {
 	static defaultProps = {
 		limit: 4
 	};
-
-	constructor(props) {
-		super(props);
-
-		this.state = _getAppState();
-		this.onChange = this.onChange.bind(this);
-	}
+	
+	state = _getAppState();
 
 	componentDidMount() {
 		API.fetchLinks();
@@ -32,10 +27,10 @@ export default class Main extends React.Component {
 		LinkStore.removeListener("change", this.onChange);
 	}
 
-	onChange() {
+	onChange = () => {
 		console.log("4. In the view");
 		this.setState(_getAppState());
-	}
+	};
 
 	render() {
 		let content = this.state.links.slice(0, this.props.limit).map(link => {
