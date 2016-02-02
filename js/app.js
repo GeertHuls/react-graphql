ac548592525ef497442c5cf280859fcd5a8d9fe3
Geert Huls <geert.huls@macadam.eu>
2016-02-02 21:52:36 +0100

    Test query using relay based on generated schema.json file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/Main";
+import Relay from "react-relay";
 
 ReactDOM.render(<Main limit={3} />, document.getElementById('react'));
+
+console.log(
+	Relay.QL`
+		query Test {
+			links {
+				title,
+				url
+			}
+		}
+	`
+);
