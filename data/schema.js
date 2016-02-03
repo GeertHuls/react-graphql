98cd8e5f12a0836b6b0e0c21df5d88ff2a43d79b
Geert Huls <geert.huls@macadam.eu>
2016-02-03 23:56:33 +0100

    Created link connection to get access to additional fetching strategies (first, last, cursor,...)

--- data/schema.js
+++ data/schema.js
@@ -3,8 +3,16 @@ import {
 	GraphQLObjectType,
 	GraphQLList,
 	GraphQLInt,
-	GraphQLString
+	GraphQLString,
+	GraphQLNonNull,
+	GraphQLID
 } from 'graphql';
+
+import {
+	connectionDefinitions,
+	connectionArgs,
+	connectionFromPromisedArray
+}from 'graphql-relay';
 
 let Schema = (db) => {
 
@@ -12,9 +20,13 @@ let Schema = (db) => {
 	let storeType = new GraphQLObjectType({
 			name: 'Store',
 			fields: () => ({
-				links: {
-					type: new GraphQLList(linkType),
-					resolve: () => db.collection("links").find({}).toArray()
+				linkConnection: {
+					type: linkConnection.connectionType,
+					args: connectionArgs, //First, last, ....
+					resolve: (_, args) => connectionFromPromisedArray(
+						db.collection("links").find({}).toArray(),
+						args
+					)
 				}
 			})
 		})
@@ -22,12 +34,20 @@ let Schema = (db) => {
 	let linkType = new GraphQLObjectType({
 		name: 'Link',
 		fields: () => ({
-			_id: { type: GraphQLString },
+			id: {
+				type: new GraphQLNonNull(GraphQLID),
+				resolve: (obj) => obj._id
+			},
 			title: { type: GraphQLString },
 			url: { type: GraphQLString }
 		})
 	});
 	
+	let linkConnection = connectionDefinitions({
+		name: 'Link',
+		nodeType: linkType
+	});
+
 	let schema = new GraphQLSchema({
 		query: new GraphQLObjectType({
 			name: 'Query',
